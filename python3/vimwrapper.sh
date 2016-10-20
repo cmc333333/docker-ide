d1cc8d3c41c0f30ebb91d31006dd2b4274f4a2b7
CM Lubinski <cm.lubinski@gmail.com>
2016-10-20 19:56:31 -0400

    Install jedi into the local venv

--- python3/vimwrapper.sh
+++ python3/vimwrapper.sh
@@ -4,16 +4,17 @@ then
   echo 'Directory is empty. Did you mount one via -v "$PWD":/workdir/ ?'
 fi
 
+# activate virtualenv
+export VIRTUAL_ENV="/workdir/.venv"
+export PATH="$VIRTUAL_ENV/bin:$PATH"
+
 # create virtualenv
 if [ ! -d .venv ]
 then
   echo 'No virtualenv; creating one. This may take a moment'
-  pyvenv .venv --system-site-packages
+  pyvenv .venv
+  pip install jedi
 fi
-
-# activate virtualenv
-export VIRTUAL_ENV="/workdir/.venv"
-export PATH="$VIRTUAL_ENV/bin:$PATH"
 
 # start vim
 vim -i /home/history/viminfo $@
